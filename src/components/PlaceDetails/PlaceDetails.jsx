b163478c943c474e8e0a548c4ada4bd01db8ca69
nikitenko <yevhenii.nikitenko@gmail.com>
2021-04-28 21:20:28 +0300

    fies

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -8,87 +8,155 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 const PlaceDetails = (props) => {
+    const [expanded, setExpanded] = React.useState(false);
+
     return (
-        <Card style={{
-            margin: '0px 10px', 
-            borderRadius: '0px',
-            borderBottom: '1px solid #adadad',
-            display: 'flex',
-            padding: '10px',
-            boxShadow: 'none'
-        }}>
-            <CardMedia
-                image="https://img.budgettravel.com/_ampArticle/foreign-currency.jpg?mtime=20190709095321"
-                title="pic"
-                style={{
-                    width: '35%'
-                }}
-            />
-            <CardContent style={{
-                // height: '100px',
-                // overflowY: 'scroll'
+        <Card
+            style={{
+                margin: '0px',
+                borderRadius: '0px',
+                borderBottom: '1px solid #adadad',
+                display: 'flex',
+                padding: '10px',
                 boxShadow: 'none',
-                width: '60%',
-                padding: '0px 8px'
-            }}>
-                <Typography style={{
-                    fontWeight: '500',
-                    fontSize: '16px',
-                    whiteSpace: 'nowrap',
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden'
-                }}>
-                    {props.exchanger.address}
-                </Typography>
-                <Typography style={{
-                    fontSize: '12px'
-                }}>
-                    {props.exchanger.name}
-                </Typography>
-                <Typography style={{
-                    fontSize: '12px',
-                    float: 'left'
-                }}>
-                    {props.exchanger.phone}
-                </Typography>
-                <Typography style={{
-                    fontSize: '12px',
-                    float: 'right'
-                }}>
-                    updated: {props.exchanger.lastUpdate || '16:50'}
-                </Typography>
-                <Grid container>
-                    <Grid
-                        container
-                        style={{ textAlign: 'center', margin: '4px 0px', padding: '4px' }}
+            }}
+        >
+            <CardMedia
+                style={{
+                    width: '300px',
+                    height: '250px',
+                }}
+                image="https://img.budgettravel.com/_ampArticle/foreign-currency.jpg?mtime=20190709095321"
+            />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: 'calc(100% - 300px)',
+                }}
+            >
+                <CardContent
+                    style={{
+                        boxShadow: 'none',
+                        padding: '0px 8px',
+                        overflowY: 'hidden',
+                        height: expanded ? 'auto' : '204px',
+                    }}
+                >
+                    <Typography
+                        style={{
+                            fontWeight: '500',
+                            fontSize: '16px',
+                            whiteSpace: 'nowrap',
+                            textOverflow: 'ellipsis',
+                            overflow: 'hidden',
+                        }}
                     >
-                        <Grid item xs>Bid</Grid>
-                        <Grid item xs>currency</Grid>
-                        <Grid item xs>Ask</Grid>
-                    </Grid>
-                    {Object.keys(props.exchanger.rates).map(
-                        (currency, index) => (
-                            <Grid
-                                container
-                                key={index}
-                                style={{ textAlign: 'center', border: '1px solid #282c34', margin: '4px 0px', padding: '4px' }}
-                            >
-                                <Grid item xs>{props.exchanger.rates[currency].bid}</Grid>
-                                <Grid item xs>{currency}</Grid>
-                                <Grid item xs>{props.exchanger.rates[currency].ask}</Grid>
+                        {props.exchanger.address}
+                    </Typography>
+                    <Typography
+                        style={{
+                            fontSize: '12px',
+                        }}
+                    >
+                        {props.exchanger.name}
+                    </Typography>
+                    <Typography
+                        style={{
+                            fontSize: '12px',
+                            float: 'left',
+                        }}
+                    >
+                        {props.exchanger.phone}
+                    </Typography>
+                    <Typography
+                        style={{
+                            fontSize: '12px',
+                            float: 'right',
+                        }}
+                    >
+                        updated: {props.exchanger.lastUpdate || '16:50'}
+                    </Typography>
+                    <Grid container>
+                        <Grid
+                            container
+                            style={{
+                                textAlign: 'center',
+                                margin: '4px 0px',
+                                padding: '4px',
+                            }}
+                        >
+                            <Grid item xs>
+                                Bid
                             </Grid>
-                        ),
-                    )}
-                </Grid>
-            </CardContent>
-            {/* <CardActions>
-                <Button size="small" color="primary">
-          Share
-                </Button>
-                <Button size="small" color="primary">
-          Learn More
-                </Button>
-            </CardActions> */}
+                            <Grid item xs>
+                                currency
+                            </Grid>
+                            <Grid item xs>
+                                Ask
+                            </Grid>
+                        </Grid>
+                        {Object.keys(props.exchanger.rates).map(
+                            (currency, index) => (
+                                <Grid
+                                    container
+                                    key={index}
+                                    style={{
+                                        textAlign: 'center',
+                                        border: '1px solid #adadad',
+                                        margin: '4px 0px',
+                                        padding: '4px',
+                                    }}
+                                >
+                                    <Grid item xs>
+                                        {props.exchanger.rates[currency].bid}
+                                    </Grid>
+                                    <Grid item xs>
+                                        {currency}
+                                    </Grid>
+                                    <Grid item xs>
+                                        {props.exchanger.rates[currency].ask}
+                                    </Grid>
+                                </Grid>
+                            ),
+                        )}
+                    </Grid>
+                </CardContent>
+
+                <div>
+                    <CardActions>
+                        {expanded ? (
+                            <Button
+                                size="small"
+                                style={{
+                                    width: '100%',
+                                    background: '#282c34',
+                                    color: 'white',
+                                }}
+                                onClick={() => {
+                                    setExpanded(false);
+                                }}
+                            >
+                                Less
+                            </Button>
+                        ) : (
+                            <Button
+                                size="small"
+                                style={{
+                                    width: '100%',
+                                    background: '#282c34',
+                                    color: 'white',
+                                }}
+                                onClick={() => {
+                                    setExpanded(true);
+                                }}
+                            >
+                                More
+                            </Button>
+                        )}
+                    </CardActions>
+                </div>
+            </div>
         </Card>
     );
 };
